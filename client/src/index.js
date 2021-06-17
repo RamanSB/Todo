0218f2af0a133747093cf51ccbf3e35529f76e4f
RamanSB <ramansb100@gmail.com>
2021-06-17 20:16:22 +0100

    Ensuring App Bar header is formatted correctly with correct stylings

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-
-function TodoInput() {
-    const [inputState, setInputState] = React.useState({
-        shouldEnterDescription: false,
-        title: "",
-        description: ""
-    });
-
-    return (
-        <div style={{width:"400px"}}>
-            <TextField 
-                id="todo-text-field" 
-                label={inputState.shouldEnterDescription ? "Description" : "Title"}
-                variant="outlined"
-                color="#ffeeaa"
-                size="medium"
-                placeholder="Todo Title"
-                fullWidth="524px"/>
-        </div>
-    )
-}
+import AppBarHeader from './components/AppBarHeader';
+import customTheme from './styles/theme.js';
+import { ThemeProvider } from '@material-ui/core/styles'
 
 const App = () => {
     console.log(`[App]`);
     return (
-        <div>
-            <h1>Todo List</h1>
-            <TodoInput/>
-        </div>
+        <ThemeProvider theme={customTheme}>
+            <AppBarHeader/>
+        </ThemeProvider>
     )
 }
 
---
+++ client/src/styles/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+
+const customTheme = createMuiTheme({
+
+});
+
+export default customTheme;
