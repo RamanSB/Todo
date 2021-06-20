7996fa4ccaf6bf31b6112bd5db4c3029ee24b850
RamanSB <ramansb100@gmail.com>
2021-06-20 19:50:43 +0100

    Restarting the UI from scratch

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import { AppBar, IconButton, Paper, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
-import AppBarHeader from './components/AppBarHeader';
-import customTheme from './styles/theme.js';
-import { ThemeProvider } from '@material-ui/core/styles'
+import { ListAlt } from '@material-ui/icons';
+import { grey } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles({
+    rootContainer: {
+        display: "flex",
+        justifyContent: "center"
+    },
+    paperContainer: {
+        width: "800px",
+        height: "640px",
+        marginTop: "80px",
+        backgroundColor: "#fefe"
+    },
+    toolbarIcon: {
+        marginLeft: "1px",
+        color: "#efefef"
+    }
+})
 
 const App = () => {
-    console.log(`[App]`);
+
+    const classes = useStyles();
+
     return (
-        <ThemeProvider theme={customTheme}>
-            <AppBarHeader/>
-        </ThemeProvider>
+        <div id="root" className={classes.rootContainer}>
+        <Paper elevation={2} className={classes.paperContainer}>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Typography variant="h6">
+                        ToDo
+                    </Typography>
+                    <IconButton edge="start" className={classes.toolbarIcon}>
+                        <ListAlt/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Paper>
+        </div>
+
+     
     )
 }
 
